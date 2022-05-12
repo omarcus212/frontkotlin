ad3eb5d972091e595c0792f538d0c133a4534329
21276377 <https://github.com/omarcus212/andoridstudio.git>
2022-05-12 16:49:57 -0300

    first commit

---
+++ app/src/main/java/com/example/splahgame/menuframe4.kt
@@ -0,0 +1,11 @@
+package com.example.splahgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menuframe4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menuframe4)
+    }
+}
